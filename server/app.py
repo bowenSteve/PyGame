f2a45a8e6c4abd87f9b06e9db4ee2dd069b8428a
stephenbowen <bowensteven17@gmail.com>
2024-07-24 15:33:14 +0300

    updates

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from .models import db, bcrypt, User, Quiz, Question, Choice, Score
+from server.models import db, bcrypt, User, Quiz, Question, Choice, Score
 
 def create_app():
     app = Flask(__name__)
